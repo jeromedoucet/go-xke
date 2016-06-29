90e8a3fa86dcf452019328c3bcd9626ae2618169
ortegadiana <dortega@xebia.fr>
2016-06-29 09:05:01 +0200

    step 1 solution

--- registration/registration.go
+++ registration/registration.go
@@ -5,19 +5,31 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/vil-coyote-acme/go-concurrency/commons"
 	"net/http"
+
+	"github.com/vil-coyote-acme/go-concurrency/commons"
 )
 
 func Register(clientUrl string, ourIp string, playerId string) error {
-	body, marshErr := json.Marshal(commons.Registration{PlayerId: playerId, Ip: ourIp})
+	/**TODO 1. use json library to marshall an object of type registration*/
+	//a. create an object type registration
+	registration := commons.Registration{PlayerId: playerId, Ip: ourIp}
+	//b. use json library to marshall the object
+	body, marshErr := json.Marshal(registration)
+	//c. if error (in marshall) return the error
 	if marshErr != nil {
 		return marshErr
 	}
-	res, httpErr := http.Post(clientUrl+"/registration", "application/json", bytes.NewBuffer(body))
+	/**TODO 2. send a post to the clientRegistrationURL with the marshalled registration, to register the client*/
+	clientRegistrationURL := clientUrl + "/registration"
+	// a. use http library to send a post of type application/json with the response of the previous step
+	res, httpErr := http.Post(clientRegistrationURL, "application/json", bytes.NewBuffer(body))
+	// b.if error (in post) , return the error
 	if httpErr != nil {
 		return httpErr
 	}
+	// c. if the StatusCode of the response is != 200 return new error
+	// hint: use error library combined with fmt to create a new error and return it
 	if res.StatusCode != 200 {
 		return errors.New(fmt.Sprintf("bad http code. Expected 200, got %d", res.StatusCode))
 	}
--- server/server_white_box_test.go
+++ server/server_white_box_test.go
@@ -1,13 +1,14 @@
 package server
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
-	"github.com/vil-coyote-acme/go-concurrency/commons"
-	"encoding/json"
 	"bytes"
-	"net/http"
+	"encoding/json"
 	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vil-coyote-acme/go-concurrency/commons"
 )
 
 func Test_NewServer(t *testing.T) {
@@ -48,6 +49,48 @@ func Test_unmarshallOrder_should_unmarshal_with_error(t *testing.T) {
 	assert.NotNil(t, err)
 }
 
+func Test_postOrder_should_fail(t *testing.T) {
+	// given
+	var order commons.Order
+	server := new(Server)
+	server.playerId = "new id"
+	// when
+	_, err := server.postOrder(order)
+	// then
+	assert.NotNil(t, err)
+}
+
+func Test_postOrder_should_do_without_error(t *testing.T) {
+	// given
+	order := commons.Order{Id: 1, Quantity: 4, CallBackUrl: "111.111.111.111", PlayerId: "a player id", Valid: true}
+	server := new(Server)
+	server.playerId = "new id"
+	server.bartenderUrl = "http://123.com"
+	// when
+	resp, err := server.postOrder(order)
+	// then
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+}
+
+func Test_getDataFromCallback_should_fail_with_error_in_url(t *testing.T) {
+	// given
+	var order commons.Order
+	// when
+	err := getDataFromCallback(order)
+	// then
+	assert.NotNil(t, err)
+}
+
+func Test_getDataFromCallback_should_not_fail(t *testing.T) {
+	// given
+	var order commons.Order
+	order.CallBackUrl = "http://123.com"
+	// when
+	err := getDataFromCallback(order)
+	// then
+	assert.Nil(t, err)
+}
 
 type nopCloser struct {
 	io.Reader
