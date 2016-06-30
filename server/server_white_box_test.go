63a59ef4163eff3b01f08c17724e75ba0d938eb5
ortegadiana <dortega@xebia.fr>
2016-06-30 19:03:17 +0200

    correction test http with mock

--- server/server_white_box_test.go
+++ server/server_white_box_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -62,10 +63,21 @@ func Test_postOrder_should_fail(t *testing.T) {
 
 func Test_postOrder_should_do_without_error(t *testing.T) {
 	// given
-	order := commons.Order{Id: 1, Quantity: 4, CallBackUrl: "111.111.111.111", PlayerId: "a player id", Valid: true}
 	server := new(Server)
 	server.playerId = "new id"
-	server.bartenderUrl = "http://123.com"
+	order := commons.Order{Id: 1, Quantity: 4, CallBackUrl: "111.111.111.111"}
+	srv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request) {
+		var recievedOrder commons.Order
+		commons.UnmarshalOrderFromHttp(rq, &recievedOrder)
+
+		assert.Equal(t, order.Id, recievedOrder.Id)
+		assert.Equal(t, server.playerId, recievedOrder.PlayerId)
+		assert.Equal(t, order.CallBackUrl, recievedOrder.CallBackUrl)
+		assert.Equal(t, order.Quantity, recievedOrder.Quantity)
+		rw.WriteHeader(200)
+	}))
+
+	server.bartenderUrl = srv.URL
 	// when
 	resp, err := server.postOrder(order)
 	// then
@@ -85,7 +97,11 @@ func Test_getDataFromCallback_should_fail_with_error_in_url(t *testing.T) {
 func Test_getDataFromCallback_should_not_fail(t *testing.T) {
 	// given
 	var order commons.Order
-	order.CallBackUrl = "http://123.com"
+	srv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request) {
+		assert.Equal(t, http.MethodGet, rq.Method)
+		rw.WriteHeader(200)
+	}))
+	order.CallBackUrl = srv.URL
 	// when
 	err := getDataFromCallback(order)
 	// then
